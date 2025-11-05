43a53d240a544c9ec25351ac2e42acdb336df552
M4rt1nG24 <140270086+M4rt1nG24@users.noreply.github.com>
2025-11-05 14:25:34 -0500

    Update login API endpoint in Login.js

--- Javascript/Login.js
+++ Javascript/Login.js
@@ -8,7 +8,7 @@ function loginUsuario() {
     const password = document.getElementById('password').value.trim();
     const mensajeLogin = document.getElementById('error-message');
 
-    fetch('https://ec2-3-129-59-49.us-east-2.compute.amazonaws.com/login', {
+    fetch('https://ptwgzsqxy4.execute-api.us-east-2.amazonaws.com/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ username, password }) 
@@ -36,6 +36,7 @@ function loginUsuario() {
         mensajeLogin.innerText = 'Error de conexión con el servidor.';
     }); 
 }
+
 
 
 
